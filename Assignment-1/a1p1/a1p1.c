5f56976e9ba169bd82d36f9b28b7ddd835668292
Kabir S. Tamari <kabirtamari@gmail.com>
2025-09-17 17:26:39 +0200

    part one completed

--- Assignment-1/a1p1/a1p1.c
+++ Assignment-1/a1p1/a1p1.c
@@ -7,18 +7,36 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "uart.h"
-#include "iregister.h"
+#include <stdlib.h>
+#include "lib/uart.h"
+#include "lib/iregister.h"
 
 #define LINE 80
+
+// Helper function to read a number using UART
+int uart_read_number() {
+    char str[LINE];
+    char c;
+    int i = 0;
+
+    while (i < LINE - 1) {
+        c = uart_getc();
+        if (c == '\n' || c == '\r') {
+            break;
+        }
+        str[i] = c;
+        i++;
+    }
+    str[i] = '\0';
+    return atoi(str);
+}
 
 int main()
 {
 	iRegister r;
-	char str[LINE];
 	char name[LINE];
 	char c;
-	int inumber, inibble, ibit, ishift = 0;
+	int initial_value, bit_pos, nibble_val, shift_val;
 	int i = 0;
 
 	// Using the uart
@@ -51,25 +69,133 @@ int main()
 	uart_puts("Hello, ");
 	uart_puts(name);
 	uart_puts("! Welcome to the Real-Time Embedded Systems course.\n");
+	uart_puts("--------------------------------\n");
 
-	// Display name length
-	uart_puts("Your name has ");
-	// Convert length to string for display
-	char length_str[10];
-	sprintf(length_str, "%d", i);
-	uart_puts(length_str);
-	uart_puts(" characters.\n");
+	// Get initial value for register
+	uart_puts("Enter initial value (decimal): ");
+	initial_value = uart_read_number();
+	r.content = initial_value;
 
-	// To Display a string
-	// uart_puts("String\n");
+	uart_puts("Initial Value: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
 
-	// To get one character
-	// c=uart_getc();
+	// Test resetBit
+	uart_puts("Enter bit position to reset (0-31): ");
+	bit_pos = uart_read_number();
 
-	// However, to get a number, you need to call uart_getc
-	// multiple times until receiving a new line.
-	// The results of each call to uart_getc can be stored into str
-	// atoi(str) will result a number.
+	uart_puts("Before reset: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	resetBit(bit_pos, &r);
+
+	uart_puts("After reset: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	// Test resetAll
+	r.content = initial_value;
+	uart_puts("Testing resetAll - Before: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	resetAll(&r);
+
+	uart_puts("After resetAll: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	// Test setBit
+	r.content = initial_value;
+	uart_puts("Enter bit position to set (0-31): ");
+	bit_pos = uart_read_number();
+
+	uart_puts("Before set: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	setBit(bit_pos, &r);
+
+	uart_puts("After set: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	// Test setAll
+	r.content = 0;
+	uart_puts("Testing setAll - Before: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	setAll(&r);
+
+	uart_puts("After setAll: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	// Test getBit
+	r.content = initial_value;
+	uart_puts("Enter bit position to read (0-31): ");
+	bit_pos = uart_read_number();
+
+	uart_puts("Register: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	print2uart("Bit at position %d: %d\n", bit_pos, getBit(bit_pos, &r));
+	uart_puts("--------------------------------\n");
+
+	// Test nibble operations
+	r.content = initial_value;
+	uart_puts("Testing nibble operations:\n");
+	uart_puts("Register: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	print2uart("Lower nibble (pos 1): %d\n", getNibble(1, &r));
+	print2uart("Upper nibble (pos 2): %d\n", getNibble(2, &r));
+
+	uart_puts("Enter value for lower nibble (0-15): ");
+	nibble_val = uart_read_number();
+	assignNibble(nibble_val, 1, &r);
+	uart_puts("After setting lower nibble: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	uart_puts("Enter value for upper nibble (0-15): ");
+	nibble_val = uart_read_number();
+	assignNibble(nibble_val, 2, &r);
+	uart_puts("After setting upper nibble: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	// Test shift operations
+	r.content = initial_value;
+	uart_puts("Testing shift operations:\n");
+	uart_puts("Initial: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	uart_puts("Enter positions to shift left: ");
+	shift_val = uart_read_number();
+	shiftLeft(shift_val, &r);
+	uart_puts("After left shift: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+
+	uart_puts("Enter positions to shift right: ");
+	shift_val = uart_read_number();
+	shiftRight(shift_val, &r);
+	uart_puts("After right shift: ");
+	uart_puts(reg2str(r));
+	print2uart(" = %d\n", r.content);
+	uart_puts("--------------------------------\n");
+
+	uart_puts("All tests completed!\n");
 
 	return 0;
 }
