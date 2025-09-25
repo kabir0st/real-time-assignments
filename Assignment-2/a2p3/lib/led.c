6ab2b82a965829d6e2b2eb6b9a31874c3f0d4ddd
Freppe98 <98frol52@live.se>
2025-09-25 14:01:36 +0200

    led update

--- Assignment-2/a2p3/lib/led.c
+++ Assignment-2/a2p3/lib/led.c
@@ -9,7 +9,7 @@
 #include "rpi-gpio.h"
 
 void led_init(){
-	/* Write 1 to the GPIO16 init nibble in the Function Select 1 GPIO
+	/* Write 1 to the GPIO36 init nibble in the Function Select 1 GPIO
        peripheral register to enable GPIO16 as an output */
     GPIO->GPFSEL1 |= (1 << 18);
 
